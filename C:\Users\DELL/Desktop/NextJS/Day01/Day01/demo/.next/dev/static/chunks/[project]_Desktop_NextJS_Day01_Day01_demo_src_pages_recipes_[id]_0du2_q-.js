09bf966d7f9e4bfdd2a3f5efaf6fe63ff7c2e395
C0derM0hamed <mohamedmostafa2011360@gmail.com>
2026-06-21 17:53:51 +0300

    delete unwanted files

--- C:\Users\DELL/Desktop/NextJS/Day01/Day01/demo/.next/dev/static/chunks/[project]_Desktop_NextJS_Day01_Day01_demo_src_pages_recipes_[id]_0du2_q-.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/0y5b_next_dist_compiled_1v5788l._.js","static/chunks/0y5b_next_dist_shared_lib_04o6a-j._.js","static/chunks/0y5b_next_dist_client_12myrid._.js","static/chunks/0y5b_next_dist_1w1rerc._.js","static/chunks/0y5b_react-dom_0fbwf49._.js","static/chunks/0y5b_13ni9j6._.js","static/chunks/[root-of-the-server]__1k1ewqd._.js"],
-    source: "entry"
-});
